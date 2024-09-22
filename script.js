1590bc3358661d991e338c98c284802785d080d4
Eng-Mohamed <mkayta2023@gmail.com>
2024-09-22 18:01:28 +0300

    I done this challenge

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Good Luck 💪🏾
+function findSeven(array) {
+
+    for (let num of array) {
+        // console.log(num)
+        if (num===7) {
+            return "Found!";
+        }
+    }
+    return "there is no 7 in the array";
+}
+
+console.log(findSeven([1, 2, 3, 4, 5, 6, ]));  
+console.log(findSeven([8, 6, 33, 100]));         
+console.log(findSeven([2, 55, 60, 7, 86]));    
